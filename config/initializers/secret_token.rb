0b44f1472a6c521089c18e889bac4f0fb6045b5b
sumansai14 <suman.sai14@gmail.com>
2013-02-21 00:39:12 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facemash::Application.config.secret_token = '5ab818ea4288ec8a733b66194a4090bd4073ddc45e33a95681fc8cdab1194b08d353e5f5f1a401a72b8df13fcbbea878c20ed8630be420e1724f3dddd330556e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Facemash::Application.config.session_store :cookie_store, key: '_facemash_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Facemash::Application.config.session_store :active_record_store
