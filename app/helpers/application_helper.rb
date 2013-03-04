cbd8ae5ad03489611934bf1f5ce886d3ee9e6f1f
sumansai14 <suman.sai14@gmail.com>
2013-03-05 02:42:43 +0530

    changes in privatepub server env

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 def broadcast(channel, &block)
   message = {:channel => channel, :data => capture(&block)}
-  uri = URI.parse("http://localhost:9292/faye")
+  uri = URI.parse("http://facemash14.herokuapp.com/faye")
   Net::HTTP.post_form(uri, :message => message.to_json)
 end
 end
